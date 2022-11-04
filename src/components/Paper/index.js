43b9bcb056c2017d922de564c58f5903f53f2ca2
rayj6 <nguyentu5526@gmail.com>
2022-11-04 23:26:34 +0700

    paper drawing function finished

---
+++ src/components/Paper/index.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import CanvasDraw from "react-canvas-draw";
+
+import "./styles.css";
+
+const Canvas = ({ color, strokeWidth }) => {
+  const [WIDTH, setWIDTH] = useState(window.innerWidth);
+  const [HEIGHT, setHEIGHT] = useState(window.innerHeight);
+
+  useEffect(() => {
+    setWIDTH(window.innerWidth);
+    setHEIGHT(window.innerHeight);
+  });
+
+  return (
+    <div>
+      <CanvasDraw
+        canvasWidth={WIDTH}
+        canvasHeight={HEIGHT}
+        brushColor={color}
+        brushRadius={strokeWidth}
+        enablePanAndZoom={true}
+        mouseZoomFactor={0.01}
+      />
+    </div>
+  );
+};
+
+const ColorPicker = ({ color, setColor }) => {
+  return (
+    <div id="colorPicker">
+      <button
+        id="white"
+        value={color}
+        onClick={() => setColor("white")}
+      ></button>
+      <button
+        id="black"
+        value={color}
+        onClick={() => setColor("black")}
+      ></button>
+      <button id="blue" value={color} onClick={() => setColor("blue")}></button>
+      <button id="red" value={color} onClick={() => setColor("red")}></button>
+      <button
+        id="green"
+        value={color}
+        onClick={() => setColor("green")}
+      ></button>
+      <button
+        id="yellow"
+        value={color}
+        onClick={() => setColor("yellow")}
+      ></button>
+      <button
+        id="purple"
+        value={color}
+        onClick={() => setColor("purple")}
+      ></button>
+      <button id="pink" value={color} onClick={() => setColor("pink")}></button>
+      <button id="grey" value={color} onClick={() => setColor("grey")}></button>
+      <input
+        type={"color"}
+        id="moreColor"
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      />
+    </div>
+  );
+};
+
+const index = () => {
+  const [color, setColor] = useState("black");
+  const [strokeWidth, setStrokeWidth] = useState(2);
+
+  return (
+    <div className="paper_container">
+      <div id="topBar">
+        <div id="logo_container">
+          <h1>Logo</h1>
+        </div>
+        <h1 id="title">title</h1>
+        <div id="participants_container">
+          <p>00 người tham gia</p>
+        </div>
+      </div>
+
+      <div id="sideFunctions">
+        <button id="undo">Undo</button>
+        <button id="clear">Clear</button>
+        <input
+          type={"number"}
+          value={strokeWidth}
+          onChange={(e) => setStrokeWidth(e.target.value)}
+        />
+      </div>
+
+      <ColorPicker color={color} setColor={setColor} />
+
+      <div id="toolBar">
+        <button id="draw">Draw</button>
+        <button id="Rubber">Rubber</button>
+        <button id="Cursor">Cursor</button>
+        <button id="Note">Note</button>
+        <button id="Image">Image</button>
+        <button id="Text">Text</button>
+      </div>
+
+      <div id="canvasContainer">
+        <Canvas color={color} strokeWidth={strokeWidth} />
+      </div>
+    </div>
+  );
+};
+
+export default index;
