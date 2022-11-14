bff4eede133ba780ced4191b103b6fbf1392c959
rayj6 <nguyentu5526@gmail.com>
2022-11-14 20:51:47 +0700

    Draw finished (not save)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 
 import Paper from "./components/Paper/index.js";
+// import Login from "./components/Login/index.js";
+// import Register from "./components/Register/index.js";
 
 function App() {
   return (
