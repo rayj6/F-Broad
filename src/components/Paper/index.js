ed78ebb9e437ccb45448f2c6fd298d3d4f113ba6
PandaKewt <chuminhtuan30306@gmail.com>
2022-11-30 22:38:37 +0700

    Add unlock and lock func

--- src/components/Paper/index.js
+++ src/components/Paper/index.js
@@ -1,17 +1,17 @@
+// Add support for tablet and mobile
+
 import React from "react";
-import {Stage, Layer, Line, Transformer} from "react-konva";
+import {Stage, Layer, Line, Rect} from "react-konva";
 import {
     onValue,
     update,
     ref,
     set,
-    // remove,
     push,
-    child,
-    // get,
 } from "firebase/database";
 import { onAuthStateChanged } from "firebase/auth";
 import { Auth, Database } from "../../firebase.config";
+import simplify from "simplify-js";
 
 import "./styles.css";
 
@@ -61,14 +61,25 @@ class Index extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            currentLine: {
+                properties: {
+                    userId: null,
+                    lock: false,
+                },
+                options: {
+                    stroke: null,
+                    strokeWidth: null,
+                },
+            },
+            lines: {},
             selectedElm: [],
             stroke: "#000000",
             strokeWidth: 5,
-            lines: [],
-            currentLine: {},
             user: null,
         };
         this.stageRef = React.createRef();
+        this.canvasContainerRef = React.createRef();
+        this.layerRef = React.createRef();
         this.clearRef = React.createRef();
         this.undoRef = React.createRef();
 
@@ -77,8 +88,10 @@ class Index extends React.Component {
         this.eraserRef = React.createRef();
         this.cursorRef = React.createRef();
         this.lockRef = React.createRef();
+        this.unlockRef = React.createRef();
         this.imageRef = React.createRef();
         this.textRef = React.createRef();
+        this.rectRef = React.createRef();
         this.refDB = ref(Database, "board");
     }
     changeMode = (mode) => {
@@ -114,33 +127,74 @@ class Index extends React.Component {
         });
 
         stageElm.on("mouseup touchend", async () => {
-            stageElm.off("mousemove touchmove mouseup touchend");
-            await update(this.refDB, {[currentLineKey]: this.state.currentLine});
-            this.setState({currentLine: {}});
+            stageElm.off("mousedown touchdown mousemove touchmove mouseup touchend");
+            let currentLine = this.state.currentLine;
+            let simplifyPoint = [];
+            for(let i = 0; i < currentLine.points.length; i += 2) {
+                simplifyPoint.push({x: currentLine.points[i], y: currentLine.points[i + 1]});
+            }
+            simplifyPoint = simplify(simplifyPoint, 1, true);
+            currentLine.points = simplifyPoint.map((point) => [point.x, point.y]).flat();
+            await update(this.refDB, {[currentLineKey]: currentLine});
+            this.setState({currentLine: {
+                properties: {
+                    userId: this.state.user.uid,
+                    lock: false,
+                },
+                options: {
+                    stroke: this.state.stroke,
+                    strokeWidth: this.state.strokeWidth,
+                }
+            }});
+            stageElm.on("mousedown touchdown", this.drawMode);
         });
     };
-    selectMode = (e) => {
-        const lineElm = e.target;
-        const lockElm = this.lockRef.current;
-        if(lineElm.attrs.lineKey !== undefined){
-            lineElm.stroke("#ff0000");
-            console.log(this.state.selectedElm);
-            if(this.state.selectedElm.some((elm) => elm.attrs.lineKey === lineElm.attrs.lineKey)) return;
-            this.setState({selectedElm  : [...this.state.selectedElm, lineElm]});
-            lockElm.onclick = () => {
-                this.state.selectedElm.forEach(async (elm) => {
-                    elm.stroke("#000000");
-                    await update(child(child(this.refDB, elm.attrs.lineKey),"properties"), {lock: true});
-                })
-                this.setState({selectedElm: []});
-            }
-        } else {
+    selectMode = () => {
+        const stageElm = this.stageRef.current;
+        const rectElm = this.rectRef.current;
+        const layerElm = this.layerRef.current;
+        let x1, y1, x2, y2;
+        x1 = x2 = stageElm.getPointerPosition().x;
+        y1 = y2 = stageElm.getPointerPosition().y;
+        rectElm.visible(true);
+        rectElm.width(0);
+        rectElm.height(0);
+        stageElm.on("mousemove touchmove", () => {
+            rectElm.visible(true);
+            x2 = stageElm.getPointerPosition().x;
+            y2 = stageElm.getPointerPosition().y;
+            rectElm.setAttrs({
+                x: Math.min(x1, x2),
+                y: Math.min(y1, y2),
+                width: Math.abs(x2 - x1),
+                height: Math.abs(y2 - y1),
+            });
+        })
+        stageElm.on("mouseup touchend", () => {
+            stageElm.off("mousemove touchmove mouseup touchend mouseout");
+            const selectedElm = layerElm.getChildren((node) => {
+                    const linePos = node.getClientRect();
+                    return node.getClassName() === "Line" && (
+                        linePos.x < Math.max(x1, x2) &&
+                        linePos.x + linePos.width > Math.min(x1, x2) &&
+                        linePos.y < Math.max(y1, y2) &&
+                        linePos.y + linePos.height > Math.min(y1, y2)
+                    )
+                }
+            );
+
             this.state.selectedElm.forEach((elm) => {
                 elm.stroke("#000000");
+            });
+
+            selectedElm.forEach((elm) => {
+                elm.attrs.stroke = "#ff0000";
             })
-            this.setState({selectedElm: []});
-        }
+            rectElm.visible(false);
+            this.setState({selectedElm});
+        })
     };
+
     clearBoard = async () => {
         const lines = this.state.lines;
         for(let key in lines){
@@ -148,23 +202,45 @@ class Index extends React.Component {
                 lines[key] = null;
             }
         }
-        set(this.refDB, lines);
+        await set(this.refDB, lines);
     };
     componentDidMount = () => {
         //Event listener for elements
+        const stageElm = this.stageRef.current;
         const selectElm = this.selectRef.current;
+        const canvasContainerElm = this.canvasContainerRef.current;
         const drawElm = this.drawRef.current;
+        const lockElm = this.lockRef.current;
+        const unlockElm = this.unlockRef.current;
         const clearElm = this.clearRef.current;
-        // console.log(selectElm);
         selectElm.onclick = () => this.changeMode(this.selectMode)
         drawElm.onclick = () => this.changeMode(this.drawMode)
         clearElm.onclick = () => this.clearBoard();
-
+        lockElm.onclick = async () => {
+            if(this.state.selectedElm.length === 0) return;
+            const lines = this.state.lines;
+            this.state.selectedElm.forEach((elm) => {
+                lines[elm.attrs.lineKey].properties.lock = true;
+            })
+            await update(this.refDB, lines);
+        }
+        unlockElm.onclick = async () => {
+            if(this.state.selectedElm.length === 0) return;
+            const lines = this.state.lines;
+            this.state.selectedElm.forEach((elm) => {
+                lines[elm.attrs.lineKey].properties.lock = false;
+            })
+            await update(this.refDB, lines);
+        }
+        
         //Event listener for firebase
         onValue(this.refDB, (snapshot) => {
             const data = snapshot.val() || {};
-            this.setState({lines: data} );
+            this.setState({lines: data});
         });
+        stageElm.width(canvasContainerElm.offsetWidth);
+        stageElm.height(canvasContainerElm.offsetHeight);
+        // stageElm.scale({ x: canvasContainerElm.offsetWidth, y: canvasContainerElm.offsetHeight });
 
         onAuthStateChanged(Auth, (user) => {
             if (user !== null) {
@@ -175,9 +251,7 @@ class Index extends React.Component {
             }
         });
     };
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        // if(prevState.lines !== this.state.lines) console.log(this.state.lines);
-    }
+
     render = () => {
         return (
             <div className="paper_container">
@@ -194,6 +268,7 @@ class Index extends React.Component {
                     <button ref={this.undoRef}>Undo</button>
                     <button ref={this.clearRef}>Clear</button>
                     <button ref={this.lockRef}>Lock</button>
+                    <button ref={this.unlockRef}>Unock</button>
                     <input
                         type={"number"}
                         value={this.state.strokeWidth}
@@ -215,40 +290,33 @@ class Index extends React.Component {
                     <button ref={this.imageRef}>Image</button>
                     <button ref={this.textRef}>Text</button>
                 </div>
-                <div id="canvasContainer">
+                <div id="canvasContainer" ref={this.canvasContainerRef}>
                     <Stage
-                        width={window.innerWidth / 1}
-                        height={window.innerHeight / 1}
+                        width={window.innerWidth}
+                        height={window.innerHeight}
                         ref={this.stageRef}
                     >
-                        <Layer>
-                            {/*{[...Object.values(this.state.lines), this.state.currentLine].map((line, i) => (*/}
-                            {/*    <Line*/}
-                            {/*        key={"line" + i}*/}
-                            {/*        points={line.points}*/}
-                            {/*        stroke={"#000000"}*/}
-                            {/*        strokeWidth={5}*/}
-                            {/*        tension={0.5}*/}
-                            {/*    />*/}
-                            {/*))}*/}
+                        <Layer ref={this.layerRef}>
                             {
                                 [...Object.entries(this.state.lines),
                                     ["currentLine", this.state.currentLine]
                                 ].map(([key, value],i) => {
-                                return (
-                                    <Line
-                                        lineKey={key}
-                                        key={"line" + i}
-                                        points={value.points}
-                                        stroke={"#000000"}
-                                        strokeWidth={5}
-                                        tension={0.5}
-                                    />
-                                )
-                            })
+                                    return (
+                                        <Line
+                                            lineKey={key}
+                                            key={"line" + i}
+                                            points={value.points}
+                                            stroke={this.state.selectedElm.some((elm) => elm.attrs.lineKey === key)? "red" : value.options.stroke}
+                                            strokeWidth={5}
+                                            tension={0.5}
+                                        />
+                                    )
+                                })
                             }
-                            <Transformer
-
+                            <Rect
+                                fill={'rgba(0,0,255,0.5)'}
+                                visible={false}
+                                ref={this.rectRef}
                             />
                         </Layer>
                     </Stage>
