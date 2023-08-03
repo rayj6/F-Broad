9727ceea5bb796949432b1f3361e86b6893250a7
rayj6 <nguyentu5526@gmail.com>
2023-08-03 14:43:14 +0700

    Done Draw and erase

--- functions/Draw.js
+++ functions/Draw.js
@@ -1,59 +1,63 @@
-function initializeDrawing() {
+let canvas = document.getElementById("drawingCanvas");
+let context = canvas.getContext("2d");
+let isDrawing = false;
+let isErasing = false;
+
+function toggleDraw() {
+    isDrawing = !isDrawing;
+    isErasing = false;
     document.body.style.cursor = 'url("../assets/board/pen-active.svg") 5 35, auto';
-    const canvas = document.getElementById("drawingCanvas");
-    const ctx = canvas.getContext("2d");
-
-    let isDrawing = false;
-    let isOverButton = false; // Flag to indicate if the pointer is over the button
-    let lastX = 0;
-    let lastY = 0;
-    let brushSize = 2; // Default brush size
-    let brushColor = "#d9d9d9"; // Default brush color
-
-    function resizeCanvas() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    }
-
-    function draw(e) {
-        if (!isDrawing || isOverButton) return; // Stop drawing if isOverButton is true
-        ctx.beginPath();
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(e.clientX, e.clientY);
-        ctx.strokeStyle = brushColor;
-        ctx.lineWidth = brushSize;
-        ctx.lineCap = "round";
-        ctx.stroke();
-        [lastX, lastY] = [e.clientX, e.clientY];
-    }
-
-    function startDrawing(e) {
-        isDrawing = true;
-        [lastX, lastY] = [e.clientX, e.clientY];
-    }
-
-    function stopDrawing() {
-        isDrawing = false;
-    }
-
-    function updateBrushSize(size) {
-        brushSize = size;
-    }
-
-    function updateBrushColor(color) {
-        brushColor = color;
-    }
-
-    resizeCanvas();
-    window.addEventListener("resize", resizeCanvas);
-
-    canvas.addEventListener("mousedown", startDrawing);
-    canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("mouseup", stopDrawing);
-    canvas.addEventListener("mouseout", stopDrawing);
-
-    return {
-        updateBrushSize,
-        updateBrushColor,
-    };
 }
+
+function toggleErase() {
+    isErasing = !isErasing;
+    isDrawing = false;
+    document.body.style.cursor = 'url("../assets/board/eraser-active.svg") 5 35, auto';
+}
+
+function draw(event) {
+    if (isDrawing) {
+        context.lineWidth = 15;
+        context.lineCap = "round"; // Set lineCap to "round" for smoother lines
+        context.strokeStyle = "#d9d9d9";
+        context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    }
+}
+
+function erase(event) {
+    if (isErasing) {
+        context.lineWidth = 25;
+        context.strokeStyle = "#1f1f1f";
+        context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    }
+}
+
+canvas.addEventListener("mousedown", (event) => {
+    context.beginPath();
+    context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    if (isDrawing) {
+        canvas.addEventListener("mousemove", draw);
+    } else if (isErasing) {
+        canvas.addEventListener("mousemove", erase);
+    }
+});
+
+canvas.addEventListener("mouseup", () => {
+    canvas.removeEventListener("mousemove", draw);
+    canvas.removeEventListener("mousemove", erase);
+});
+
+// Handle canvas resizing
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas(); // Set initial canvas size
